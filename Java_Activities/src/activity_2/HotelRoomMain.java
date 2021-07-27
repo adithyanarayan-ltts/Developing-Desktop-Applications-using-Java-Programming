d2bcca87fd9c7dc52def48fff14e1a73df81ff1e
Adithya Narayan K A <adithya.ka@ltts.com>
2021-07-28 02:17:00 +0530

    Inheritance Activity 1 and 2

---
+++ Java_Activities/src/activity_2/HotelRoomMain.java
@@ -0,0 +1,127 @@
+package activity_2;
+import java.util.*;
+class HotelRoom {
+    protected String hotelName;
+    protected int numberOfSqFeet;
+    protected boolean hasTV;
+    protected boolean hasWifi;
+
+    public HotelRoom(String hotelName,int numberOfSqFeet,boolean hasTV,boolean hasWifi){
+        this.hotelName=hotelName;
+        this.numberOfSqFeet=numberOfSqFeet;
+        this.hasTV=hasTV;
+        this.hasWifi=hasWifi;
+    }
+    public int calculateTariff(int rate){
+        return rate*numberOfSqFeet;
+    }
+    public int getRatePerSqFeet(){
+        return 0;
+    }
+    
+}
+
+class DeluxeRoom extends HotelRoom {
+    protected int ratePerSqFeet;
+    
+    public  DeluxeRoom(String hotelName,int numberOfSqFeet,boolean hasTV,boolean hasWifi){
+        super(hotelName,numberOfSqFeet,hasTV,hasWifi);
+        ratePerSqFeet=10;
+        
+       
+    }
+    public int getRatePerSqFeet(){
+        if(hasWifi){
+            return ratePerSqFeet+2;
+        }
+        else{
+            return ratePerSqFeet;
+        }
+    }
+
+
+    
+}
+
+class DeluxeAcRoom extends DeluxeRoom {
+    public DeluxeAcRoom(String hotelName,int numberOfSqFeet,boolean hasTV,boolean hasWifi){
+        super(hotelName, numberOfSqFeet, hasTV, hasWifi);
+        ratePerSqFeet=12;
+    }
+}
+
+class SuiteACRoom extends HotelRoom {
+    private int ratePerSqFeet;
+    public SuiteACRoom(String hotelName,int numberOfSqFeet,boolean hasTV,boolean hasWifi){
+        super(hotelName, numberOfSqFeet, hasTV, hasWifi);
+        ratePerSqFeet=15;
+        
+
+    }
+    public int getRatePerSqFeet(){
+        if(hasWifi){
+            return ratePerSqFeet+2;
+        }
+        else{
+            return ratePerSqFeet;
+        }
+    }
+    
+}
+    
+public class HotelRoomMain {
+	public static void main(String[] args) {
+        System.out.println("Hotel Tariff Calculator");
+        System.out.println("1. Deluxe Room");
+        System.out.println("2. Deluxe AC Room");
+        System.out.println("3. Suite AC Room");
+        System.out.println("Select Room Type:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==1){
+            System.out.println("Hotel Name:");
+            String name=sc.next();
+            System.out.println("Room Square Feet Area:");
+            int area=sc.nextInt();
+            System.out.println("Room has TV(yes/no):");
+            boolean tv=(sc.next().equals("yes"))?true:false;;
+            System.out.println("Room has Wifi(yes/no):");
+            boolean wifi=(sc.next().equals("yes"))?true:false;;
+            DeluxeRoom r= new DeluxeRoom(name,area,tv,wifi);
+            int rate=r.getRatePerSqFeet();
+            System.out.println("Room Tariff per day is:"+r.calculateTariff(rate));
+
+        }
+        else if(n==2){
+            System.out.println("Hotel Name:");
+            String name=sc.next();
+            System.out.println("Room Square Feet Area:");
+            int area=sc.nextInt();
+            System.out.println("Room has TV(yes/no):");
+            boolean tv= (sc.next().equals("yes"))?true:false;
+            System.out.println("Room has Wifi(yes/no):");
+            boolean wifi= (sc.next().equals("yes"))?true:false;
+            DeluxeAcRoom d=new DeluxeAcRoom(name,area,tv,wifi);
+            int rate=d.getRatePerSqFeet();
+            System.out.println("Room Tariff per day is:"+d.calculateTariff(rate));
+            
+        }
+        else if(n==3){
+            System.out.println("Hotel Name:");
+            String name=sc.next();
+            System.out.println("Room Square Feet Area:");
+            int area=sc.nextInt();
+            System.out.println("Room has TV(yes/no):");
+            boolean tv=(sc.next().equals("yes"))?true:false;
+            System.out.println("Room has Wifi(yes/no):");
+            boolean wifi=(sc.next().equals("yes"))?true:false;
+            SuiteACRoom s=new SuiteACRoom(name,area,tv,wifi);
+            int rate=s.getRatePerSqFeet();
+            System.out.println("Room Tariff per day is:"+s.calculateTariff(rate));
+            
+
+
+        }
+        sc.close();
+    }
+}
---
+++ Java_Activities/src/activity_2/VehicleMain.java
@@ -0,0 +1,134 @@
+package activity_2;
+
+import java.util.*;
+
+class Vehicle {
+    protected String make,vechileNumber,fuelType;
+    protected int fuelCapacity,cc;
+    
+    Vehicle(String make,String vechileNumber,String fuelType,int fuelCapacity,int cc){
+        this.make=make;
+        this.vechileNumber=vechileNumber;
+        this.fuelType=fuelType;
+        this.fuelCapacity=fuelCapacity;
+        this.cc=cc;
+    }
+    public void displayMake(){
+        System.out.println("***"+make+"***");
+        
+    }
+    public void displayBasicInfo(){
+        System.out.println("---Basic Information---");
+        System.out.println("Vehicle Number: "+vechileNumber);
+        System.out.println("Fuel Capacity: "+fuelCapacity);
+        if(fuelType.equals("1"))
+        System.out.println("Fuel Type: "+"Petrol");
+        else System.out.println("Fuel Type: "+"Diesel");
+        System.out.println("CC: "+cc);
+    }
+    public void displayDetailInfo(){
+
+    }
+    
+}
+
+class TwoWheeler extends Vehicle{
+    private boolean kickStartAvailable;
+    TwoWheeler(String make,String Vnumber,String fType,int cap,int cc,boolean kickStartAvailable){
+        super(make,Vnumber,fType,cap,cc);
+        this.kickStartAvailable=kickStartAvailable;
+    }
+    public void displayDetailInfo(){
+        System.out.println("---Detail Informaion---");
+        if(kickStartAvailable)
+        System.out.println("Kick Start AVAILBLE: "+"YES");
+        else{
+            System.out.println("Kick Start AVAILBLE: "+"NO");
+        }
+    }
+   
+}
+
+class FourWheeler extends Vehicle {
+    private String audioSystem;
+    private int numberOfDoors;
+    
+     FourWheeler(String make,String Vnumber,String fType,int cap,int cc,String audioSyste,int numberOfDoors){
+    super(make,Vnumber,fType,cap,cc);
+    this.audioSystem=audioSyste;
+    this.numberOfDoors=numberOfDoors;
+}
+    public String getAudio(){
+        return this.audioSystem;
+    }
+    public int getNumber(){
+        return this.numberOfDoors;
+    }
+    
+       public void displayDetailInfo(){
+        System.out.println("---Detail Informations---");
+        System.out.println("Audio System: "+audioSystem);
+        System.out.println("Number of Doors "+numberOfDoors);
+    }
+    
+}
+public class VehicleMain {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("1. Four Wheeler");
+        System.out.println("2. Two Wheeler");
+        System.out.println("Enter Vehicle Type");
+        int type=sc.nextInt();
+        if(type==1){
+            System.out.println("Vehicle Make:");
+            String make=sc.nextLine();
+            sc.nextLine();
+            System.out.println("Vehicle Number:");
+            String Vnumber=sc.nextLine();
+            System.out.println("Fuel type:");
+            System.out.println("1. Petrol");
+            System.out.println("2. Diesel");
+            String fType=sc.nextLine();
+            System.out.println("Fuel Capacity:");
+            int cap=sc.nextInt();
+            System.out.println("Engine CC:");
+            int cc=sc.nextInt();
+           
+           System.out.println("Audio System: ");
+           String au=sc.nextLine();
+           sc.nextLine();
+           System.out.println("Number of Doors: ");
+           int doo=sc.nextInt();
+          Vehicle c=new FourWheeler(make,Vnumber,fType,cap,cc,au,doo);
+           c.displayMake();
+           c.displayBasicInfo();;
+           c.displayDetailInfo();
+        }
+        else{
+            System.out.println("Vehicle Make:");
+            String make=sc.next();
+            sc.nextLine();
+            System.out.println("Vehicle Number:");
+            String Vnumber=sc.nextLine();
+            System.out.println("Fuel type:");
+            System.out.println("1. Petrol");
+            System.out.println("2. Diesel");
+            String fType=sc.next();
+            System.out.println("Fuel Capacity:");
+            int cap=sc.nextInt();
+            System.out.println("Engine CC:");
+            int cc=sc.nextInt();
+           
+            System.out.println("Kick Start Available(yes/no):");
+            boolean ans= (sc.next().equals("yes"))?true:false;
+            Vehicle v=new TwoWheeler(make, Vnumber, fType, cap, cc, ans);
+            v.displayMake();
+            v.displayBasicInfo();
+            v.displayDetailInfo();
+
+
+        }
+        sc.close();
+    }
+    
+}
